f62af217ba705dd9caf556dd8b5653873e2860e8
cistercii <cistercii@mail.ru>
2023-03-04 12:35:12 +0300

    Подкорректировал код и перебросил его в Main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,7 @@
-// Объявляете переменные для входных данных и
-// параметров программы: одну для хранения
-// стоимости билета, другую для хранения количества
-// рублей для одной бонусной мили
-
-// Рассчитываете количество бонусных миль, используя
-// значения заведённых переменных. Ответ сохраняете в
-// новую переменную и выводите на экран
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите стоимость билета:");
-        int userInput = scanner.nextInt();
+        int userInput = 1120;
         int quantityBonus = userInput / 20;
         System.out.println("Вам начислено " + quantityBonus + " бонусов");
     }
